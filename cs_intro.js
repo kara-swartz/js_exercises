281a8733a8e60e2e2b4d11669b1aeaa1858be7f9
calogero.kz <calogero.kz@gmail.com>
2019-03-04 03:15:18 +0100

    add task about scopes and closures

--- cs_intro.js
+++ cs_intro.js
@@ -169,7 +169,7 @@ const isPrime = (n) => {
   if (n <= 1) {
        return false;
   }
-  
+   
   
   for (let counter = 2; counter < n; counter++) {
     if (n%counter===0) {
@@ -216,3 +216,77 @@ const solution = n => getTriangleArea(n, square(n) / 2);
 
 export default solution;
 // END
+
+
+
+
+
+
+/*
+Функции lessThan, greaterThan и isEqual в модуле comparers сравнивают две строки и возвращают true/false.
+
+Сравнение идет по количеству заглавных символов в строке (больше заглавных — больше строка).
+
+Специальные символы (например, пробел) не имеют заглавных эквивалентов и в данном задании считаются заглавными.
+
+Примеры:
+
+greaterThan('AD', 'ad sd'); // true, сравнение на > (больше)
+greaterThan('AD', '   Ad sd'); // false, сравнение на > (больше)
+lessThan('ghe df', 'dfwe r D'); // true, сравнение на < (меньше)
+isEqual('liSp', 'lisP'); // true
+comparers.js
+Допишите необходимые части функций bigLettersCount и compare для того, чтобы заработали функции lessThan, greaterThan и isEqual.
+
+Функция compare, принимающая две строки first и second, работает по следующему алгоритму:
+
+Если в первой строке больше заглавных символов, то возвращается 1.
+Если во второй строке больше заглавных символов, то возвращается -1.
+Иначе возвращается 0.
+Подсказки
+Функция bigLettersCount должна принимать на вход строку str и высчитывать количество заглавных символов в ней.
+Вычисление длины строки: length(str).
+Перевод строки в верхний регистр: toUpperCase(str).
+Проверка на то, что символ в верхнем регистре: toUpperCase(char) === char;
+*/
+
+import { length, toUpperCase } from './strings';
+
+const bigLettersCount = (str) => {
+  // BEGIN (write your solution here)
+  let counter = 0;
+
+  for (let i=0; i<str.length; i+=1) {	
+  	if (toUpperCase(str[i]) === str[i]) {
+  		counter += 1;
+  	}
+  }
+
+  return counter;
+  // END
+};
+
+
+const compare = (first, second) => {
+  const firstCount = bigLettersCount(first);
+  const secondCount = bigLettersCount(second);
+
+  // BEGIN (write your solution here)
+  if (firstCount > secondCount) {
+  	return 1;
+  } else if (firstCount < secondCount) {
+  	return -1;
+  } else {
+  	return 0;
+  }
+  // END
+};
+
+export const greaterThan = (first, second) => (
+  compare(first, second) === 1);
+
+export const lessThan = (first, second) => (
+  compare(first, second) === -1);
+
+export const isEqual = (first, second) => (
+  compare(first, second) === 0);
