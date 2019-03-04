434006eaf75be202684c2bc0f8b7ed29840d2905
calogero.kz <calogero.kz@gmail.com>
2019-03-04 23:23:28 +0100

    add task about data type

--- cs_intro.js
+++ cs_intro.js
@@ -290,3 +290,86 @@ export const lessThan = (first, second) => (
 
 export const isEqual = (first, second) => (
   compare(first, second) === 0);
+
+
+/*
+addDigits.js
+Реализуйте и экспортируйте по умолчанию функцию addDigits, которая работает следующим образом:
+
+Дано неотрицательное целое число num. Складывать все входящие в него цифры до тех пор, пока не останется одна цифра.
+
+Для числа 38 процесс будет выглядеть так:
+
+3 + 8 = 11
+1 + 1 = 2
+Результат: 2
+
+Пример:
+
+addDigits(10); // 1
+addDigits(19); // 1
+addDigits(38); // 2
+addDigits(1259); // 8
+Подсказка
+Выделите процесс суммирования цифр в числе в отдельную функцию
+*/
+
+import { length } from './strings'; // eslint-disable-line
+
+// BEGIN (write your solution here)
+
+const addDigits = (str) => {
+
+  const sumDigits = (str) => {
+
+    const strDigit = String(str);
+	  let digits = 0;
+
+    for (let i=0; i<length(strDigit); i+=1) {
+      digits += Number(strDigit[i]);      
+	  }
+
+    return Number(digits);
+  }
+
+  let out = String(sumDigits(str));
+  if (length(out)==1) {
+    return Number(out);
+  } else {
+    return addDigits(out);
+  }
+
+}
+	
+export default addDigits;
+// END
+
+/*
+Решение учителя:
+// BEGIN
+const sum = (str) => {
+  let result = 0;
+  for (let i = 0; i < length(str); i += 1) {
+    result += Number(str[i]);
+  }
+
+  return result;
+};
+
+const addDigits = (num) => {
+  let result = num;
+  while (result >= 10) {
+    result = sum(String(result));
+  }
+
+  return result;
+};
+
+export default addDigits;
+// END
+*/
+
+
+
+
+
