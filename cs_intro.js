bf6273abd02605215eacf4a356ee2dee364e6fff
calogero.kz <calogero.kz@gmail.com>
2019-03-06 01:00:45 +0100

    pure fucntion

--- cs_intro.js
+++ cs_intro.js
@@ -405,3 +405,19 @@ return edStr;
 export default wordUpperCase;
 //wordUpperCase("hello, world");
 // END
+
+
+/*
+Решение учителя:
+*/
+// BEGIN
+export default (str) => {
+  let result = '';
+  for (let i = 0; i < length(str); i += 1) {
+    const shouldBeBig = str[i] !== ' ' && (i === 0 || str[i - 1] === ' ');
+    result += shouldBeBig ? toUpperCase(str[i]) : str[i];
+  }
+
+  return result;
+};
+// END
